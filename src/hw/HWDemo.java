ecdf616ce711dd0f2d5c0c555ffd2373f792751b
khsed <khsed@LAPTOP-A556GNJ9>
2023-07-31 11:18:56 -0400

    My HW is added.

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -8,6 +8,9 @@ public class HWDemo {
 		System.out.println("Hello world");
 		
 		System.out.println("my name is rohullah");
+		
+		System.out.println("Hey guy, this is Khwaja, i just did my HW part, sorry guys coudnt"
+				+ " communicated over the whatsApp as i am busy moving to a new address");
 	}
 	public static void add( int a, int b) {
 		system.out.println(a+b);
