5796f12f41de0eafdf7f43c7442a060a1c261329
Rohulla123 <rohullah.asify@gmail.com>
2023-07-31 09:08:09 -0400

    added my name

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,5 +4,9 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HW");
+		
+		System.out.println("Hello world");
+		
+		System.out.println("my name is rohullah");
 	}
 }
